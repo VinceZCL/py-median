1c809845b1d10e5f7ebd40d33a4dcb8e2f38379d
VinceZCL <64774232+VinceZCL@users.noreply.github.com>
2021-03-18 16:41:36 +0800

    Update median.py

--- median.py
+++ median.py
@@ -1,17 +1,23 @@
 # Find median from data set
 
+# import rounding functions
 from math import ceil, floor
 
+# median function
 def median(data):
     length = len(data)
+    # check if length of data set is odd
     if length%2 != 0:
         mid = floor(length/2)
         print(data[mid])
     else:
+        # if length of data set is even
+        # rounding mid value
         top = ceil((length+1)/2)
         bot = floor((length+1)/2)
         topv = data[top-1]
         botv = data[bot-1]
         print((topv + botv) / 2)
 
+# calling median function
 median([1,2,3,4,5,6,7,8,9,10])
